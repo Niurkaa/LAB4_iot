0f4454925424d00cfeed342c269c888fc489b117
niurkaa <niurka_30@outlook.es>
2023-05-29 17:27:10 -0500

    vistas

---
+++ app/src/main/java/com/example/lab4_iot/verInformacion.java
@@ -0,0 +1,14 @@
+package com.example.lab4_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class verInformacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_informacion);
+    }
+}
