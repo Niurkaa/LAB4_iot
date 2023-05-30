b5719c2c30272e6adf741240ff849c1707847b6c
david34Y <dgonzalesn@pucp.edu.pe>
2023-05-30 11:06:09 -0500

    Flujo de tutor con Notificaciones

--- app/src/main/java/com/example/lab4_iot/AsignarTutoria.java
+++ app/src/main/java/com/example/lab4_iot/AsignarTutoria.java
@@ -1,7 +1,18 @@
 package com.example.lab4_iot;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -33,6 +44,8 @@ public class AsignarTutoria extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build().create(EmployeeRepository.class);
 
+        createNotificationChannel();
+
         binding.button.setOnClickListener(view -> {
             String texto1 = binding.idEmpleado.getText().toString();
             int idEmpleado = Integer.parseInt(texto1);
@@ -48,14 +61,15 @@ public class AsignarTutoria extends AppCompatActivity {
 
                         if (employee.getManagerId().toString().equals(texto2)){
                             if (citas<=0){
-                                binding.textView21.setText("Asignación de cita correcta");
+
+                                lanzarNotificacion("Asignación de cita correcta");
                                 citas++;
                             }else{
-                                binding.textView21.setText("El trabajador ya tiene una cita " + "asignada. Elija otro trabajador");
+                                lanzarNotificacion("El trabajador ya tiene una cita asignada. Elija otro trabajador");
                             }
 
                         }else{
-                            binding.textView21.setText("No es manager del empleado");
+                            lanzarNotificacion("No es manager del empleado");
                         }
 
                     } else {
@@ -70,5 +84,55 @@ public class AsignarTutoria extends AppCompatActivity {
             });
 
         });
+    }
+
+    String channelId = "channelDefaultPri";
+
+    public void createNotificationChannel() {
+        //android.os.Build.VERSION_CODES.O == 26
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Canal notificaciones default",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Canal para notificaciones con prioridad default");
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            askPermission();
+
+        }
+    }
+
+    public void askPermission(){
+        //android.os.Build.VERSION_CODES.TIRAMISU == 33
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                        PackageManager.PERMISSION_DENIED) {
+
+            ActivityCompat.requestPermissions(AsignarTutoria.this,
+                    new String[]{POST_NOTIFICATIONS},
+                    101);
+        }
+    }
+
+
+    public void lanzarNotificacion(String contenttitle) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(contenttitle)
+                .setContentText("Disfrute de su instancia")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        if (ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(1, builder.build());
+        }
     }
 }
--- app/src/main/java/com/example/lab4_iot/MainActivity.java
+++ app/src/main/java/com/example/lab4_iot/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.lab4_iot;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.lab4_iot.databinding.ActivityFlujoTutorBinding;
@@ -17,9 +27,67 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        createNotificationChannel();
+
         binding.tutor.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this,FlujoTutor.class);
             startActivity(intent);
+            lanzarNotificacion();
         });
+
+
     }
+
+    String channelId = "channelDefaultPri";
+
+    public void createNotificationChannel() {
+        //android.os.Build.VERSION_CODES.O == 26
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Canal notificaciones default",
+                    NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Canal para notificaciones con prioridad default");
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+
+            askPermission();
+
+        }
+    }
+
+    public void askPermission(){
+        //android.os.Build.VERSION_CODES.TIRAMISU == 33
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                        PackageManager.PERMISSION_DENIED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{POST_NOTIFICATIONS},
+                    101);
+        }
+    }
+
+
+    public void lanzarNotificacion() {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Estás entrando a modo tutor")
+                .setContentText("Disfruta tu instancia")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        if (ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(1, builder.build());
+        }
+    }
+
+
 }
